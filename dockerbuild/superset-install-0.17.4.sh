e1b55946504d220f6dd8776e325a12f3d06a19bd
Mário de Sá Vera <mario.vera@b2wdigital.com>
2017-11-12 17:19:18 -0200

    ICON replaced

--- dockerbuild/superset-install-0.17.4.sh
+++ dockerbuild/superset-install-0.17.4.sh
@@ -34,12 +34,12 @@ pip install MySQL-python
 # this is not necessary as the metadata database is already configured !
 #
 # Create an admin user (you will be prompted to set username, first and last name before setting a password)
-fabmanager create-admin --app superset
+#fabmanager create-admin --app superset
 
 # Initialize the database
-superset db upgrade
+#superset db upgrade
 
 # Load some data to play with
-superset load_examples
+#superset load_examples
 
 
